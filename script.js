4f52ad37381e5f3afde075aa88957344d6cdbe74
raitulsams <raitulsams202@gmail.com>
2022-02-12 03:54:54 +0600

    new-device-login

---
+++ script.js
@@ -0,0 +1,40 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    getDisplayNumber(number) {
+
+    }
+
+    updateDisply(number) {
+
+    }
+}
+const numberButtons = document.getElementById("data-number")
+const operationButtons = document.getElementById("data-operaion")
+const equalButton = document.getElementById("data-equal")
+const deleteButton = document.getElementById("data-delete")
+const allClearButton = document.getElementById("data-clear")
+const previousOperandTextElement = document.getElementById("data-previous-operand")
+const currentOperandTextElement = document.getElementById("data-current-operand")
